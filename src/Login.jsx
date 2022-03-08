df09ad015b5b4396dde68d3b597008745d9faa9b
fuse28 <mixfuse92@gmail.com>
2022-03-08 20:36:17 +0530

    first commit

---
+++ src/Login.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import "./Login.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+function Login() {
+  const [showSignup, setShowSignup] = useState(false);
+  console.log(showSignup);
+
+  const toggleSignup = () => {
+    setShowSignup(!showSignup);
+  };
+
+  return (
+    <div id="loginpage">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h2 className="home-title text-center">Welcome to ecommerce</h2>
+            {!showSignup ? (
+              <div className="login-wrapper">
+                <h4 className="text-center">Login</h4>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Username"
+                    id="username"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    type="password"
+                    classname="form-control"
+                    placeholder="Password"
+                    id="password"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    type="submit"
+                    classname="form-control btn btn-primary"
+                    value="Login as user"
+                    placeholder="Password"
+                    id="password"
+                  />
+                </div>
+                <div
+                  className="signup-btn text-center text-info"
+                  onClick={toggleSignup}
+                >
+                  Don't have a account? Sign up here
+                </div>
+                <div className="auth-error-msg text-danger text-center"></div>
+              </div>
+            ) : (
+              <div className="login-wrapper">
+                <h4 className="text-center">Sign Up</h4>
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Username"
+                    id="username"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    type="password"
+                    classname="form-control"
+                    placeholder="Password"
+                    id="password"
+                  />
+                </div>
+                <div className="input-group">
+                  <input
+                    type="submit"
+                    classname="form-control btn btn-primary"
+                    value="Signup as user"
+                    placeholder="Password"
+                    id="password"
+                  />
+                </div>
+                <div
+                  className="signup-btn text-center text-info"
+                  onClick={toggleSignup}
+                >
+                  Have a account? Login here
+                </div>
+                <div className="auth-error-msg text-danger text-center"></div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
